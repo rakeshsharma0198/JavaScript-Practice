2f54f47998d05347d4eef773b15cd651d67d0d63
Edxael <zerphyr.r@gmail.com>
2017-12-21 18:18:59 -0700

    add alg 01.js

--- 01-Algo/01.js
+++ 01-Algo/01.js
@@ -6,19 +6,16 @@ const t1 = (s) => {
   console.log(s);
   let obj = {}
 
-  // const myAns =
-
   return s.split("").map((x) => {
 
-                              if(obj.hasOwnProperty(x)){
-                                return x
-                              }else{
-                                obj[x] = 1
-                              }
+    if(obj.hasOwnProperty(x)){
+      return x
+    }else{
+      obj[x] = 1
+    }
 
   }).filter((x) => { return x !== undefined })[0]
-
- }
+}
 
 
 console.log( "The answer is: ", t1(s1) );
