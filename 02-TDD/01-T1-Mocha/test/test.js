7f88115c2398f10c5545b71d6640c4d9cf079db4
Edxael <zerphyr.r@gmail.com>
2017-12-22 19:16:04 -0700

    add second case senario for test

--- 02-TDD/01-T1-Mocha/test/test.js
+++ 02-TDD/01-T1-Mocha/test/test.js
@@ -11,4 +11,13 @@ describe('addition Test', () => {
 
     actualResult.should.equal(expectedValue)
   })
+
+  it('should return 8 when parameter values are: 5 & 3', () => {
+    const num1 = 5
+    const num2 = 3
+    const expectedValue = 8
+    const actualResult = add(num1, num2)
+
+    actualResult.should.equal(expectedValue)
+  })
 })
