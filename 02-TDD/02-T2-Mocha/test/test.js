bc2e5c1aaf3765c407533dfcb45968001be864ac
Edxael <zerphyr.r@gmail.com>
2017-12-22 22:47:21 -0700

    add new test

--- 02-TDD/02-T2-Mocha/test/test.js
+++ 02-TDD/02-T2-Mocha/test/test.js
@@ -4,6 +4,7 @@
 const chai = require('chai')
 const should = chai.should()
 const expect = chai.expect
+const assert = chai.assert
 
 const add = require('../math.js')
 
@@ -26,13 +27,22 @@ describe('test for add function', () => {
     unitResult.should.equal(expectedValue)
   })
 
-  it('expect 10 when given value 4 & 6 ', () => {
+  it('expect 10 when given values 4 & 6 ', () => {
     const value1 = 4
     const value2 = 6
     const expectedValue = 10
 
     const unitResult = add( value1, value2 )
     expect(unitResult).to.equal(expectedValue)
+  })
+
+  it('assert equality between expectedValue = 16 and unitResult when given values: 11 & 5', () => {
+    const value1 = 11
+    const value2 = 5
+    const expectedValue = 16
+
+    const unitResult = add( value1, value2 )
+    assert.equal(unitResult, expectedValue, " Test not passed ;( ")
   })
 
 })
