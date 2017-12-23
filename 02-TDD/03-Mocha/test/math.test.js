b8c9d47f8d48b014cc02efe6bca76d8eea8a8ee3
Edxael <zerphyr.r@gmail.com>
2017-12-23 12:59:50 -0700

    js practice

---
+++ 02-TDD/03-Mocha/test/math.test.js
@@ -0,0 +1,15 @@
+const add = require('../math.js')
+const chai = require('chai')
+
+const should = chai.should()
+const expect = chai.expect
+const assert = chai.assert
+
+
+describe('test for add function', () => {
+  it('shold return 7 when given values: 3 & 4', () => {
+    const value1 = 3
+    const value2 = 4
+    const expectedValue
+  })
+})
