85d6cc12651b753f2da070722da0e39f82d5ef5a
Edxael <zerphyr.r@gmail.com>
2017-12-27 14:18:30 -0700

    EAP 1

--- 03-JSF/00-code.js
+++ 03-JSF/00-code.js
@@ -1,2 +1,25 @@
+// Fiz Buzz
 
-console.log("Hello 2");
+console.log("Hello 2")
+
+for(let x = 0; x < 101; x++){
+  if( (x % 3 == 0) && (x % 5 === 0) ){
+    console.log("The number: " + x + " is Fizz-Buzz");
+  } else if (x % 3 == 0) {
+    console.log("The number: " + x + " is Fizz");
+  }else if (x % 5 == 0) {
+    console.log("The number: " + x + " is Buzz");
+  }else {
+    console.log("The number: " + x + " NA");
+  }
+}
+
+
+// Not using For Loops
+const fb = new Array(101).fill("a").map(
+  (c, i, ) => { console.log(
+      ((i % 3 === 0) && (i % 5 === 0) ) ? `Number ${i} is Fiz-Buzz`  :
+       (i % 3 === 0) ? `Number ${i} is Fizz`  :
+       (i % 5 === 0) ? `Number ${i} is Buzz`  : `Number ${i} NA`
+   ) }
+)
