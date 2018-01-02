6049d1c278650fb35e2867f5be537004b8900e69
Edxael <zerphyr.r@gmail.com>
2018-01-02 00:46:58 -0700

    start in spiral

--- 00-AP/matrix/index.js
+++ 00-AP/matrix/index.js
@@ -15,6 +15,38 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+  let counter = 0
+  let strow = 0, endrow = (n - 1)
+  let stcol = 0, endcol = (n - 1)
+
+
+  let results = Array(n).fill(1).map((x) => { return [] })
+
+  
+
+  return results
+}
+
+
+
+
+
+console.log( matrix(2) );
 
 module.exports = matrix;
+
+// create an array with values from 1 to (n * n)
+// create two dimentional matrix array fill with "x"
+
+
+
+// let arr1 = subArr(Array(n * n).fill("x"), n)
+//
+// const subArr = (arr1, n) => {
+//   let tempArr = []
+//   while(arr1.length > 0){
+//     tempArr.push(arr1.splice(0, n))
+//   }
+//   return tempArr
+// }
