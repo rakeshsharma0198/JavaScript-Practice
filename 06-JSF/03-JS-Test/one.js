48c040731b6489050daf234eed765815e35ba4d8
Edxael <zerphyr.r@gmail.com>
2018-01-14 00:07:58 -0700

    add code

--- 06-JSF/03-JS-Test/one.js
+++ 06-JSF/03-JS-Test/one.js
@@ -1,15 +1,7 @@
 console.log("\n=====================================\n");
 // ------------------------------------------------------------
 
-const str = "cleopatra"
-console.log(str)
-// console.log( str.endsWith("p", 5) )
-// console.log( str.endsWith("a") );
-
-console.log(str.repeat(3));
-
-
-
+console.log("History so Far....");
 
 
 // ------------------------------------------------------------
