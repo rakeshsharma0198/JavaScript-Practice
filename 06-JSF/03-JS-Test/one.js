bf3fcfde14abdb6eb1822c8ddca5a6c99f13da90
Edxael <zerphyr.r@gmail.com>
2018-01-15 13:18:55 -0700

    spread operator

--- 06-JSF/03-JS-Test/one.js
+++ 06-JSF/03-JS-Test/one.js
@@ -2,8 +2,17 @@ console.clear()
 console.log("\n=====================================\n");
 // ------------------------------------------------------------
 
-console.log("History so Far....  47");
+const myRest = (a, b, c, ...wakabi) => {
+  console.log(a + b +c)
+  console.log( ...wakabi );
+}
 
+
+let a = 1,  b = 2, c = 3, d = 4, e = 5, f = 6, g = ["a", 12, "cat"]
+myRest(a, b, c, d, e, f, g)
+
+
+// console.log( myRest(a, b, c, d, e, f, g) )
 
 // ------------------------------------------------------------
 console.log("\n=====================================\n");
