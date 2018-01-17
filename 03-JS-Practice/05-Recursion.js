1bb25cd84bfa08ee111198bb964d0ecb879d1ece
Edxael <zerphyr.r@gmail.com>
2018-01-17 16:29:12 -0700

    recursive fun questions

---
+++ 03-JS-Practice/05-Recursion.js
@@ -0,0 +1,37 @@
+
+// --------------------------------------------------------------------------
+// Using recursion to count number of characters in a string.
+
+const myRec = (str, num) => {
+  if(num >= str.length + 1){ return num }
+  console.log(num)
+  // myRec(str, num + 1)
+  myRec(str, num + 1)
+}
+
+console.log("Value return: ", myRec("1234567", 0) )
+
+
+// --------------------------------------------------------------------------
+// Using Recursion to add the values in one array.
+
+const myRec = (arr, total) => {
+
+  if(arr.length > 0){
+    total += arr[0]
+    arr.shift()
+    console.log(arr)
+    console.log(total)
+    console.log(" ")
+    myRec(arr, total)
+  }else {
+    console.log("returning total: ", total)
+    return total
+  }
+
+}
+
+const arr = [1, 2, 3, 4]
+console.log("What I got: ", myRec(arr, 0) )
+
+// --------------------------------------------------------------------------
