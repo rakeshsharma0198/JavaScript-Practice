7447853da8d264c14c3b96138a0efdae6c5e88bb
Edxael <zerphyr.r@gmail.com>
2018-01-18 11:46:19 -0700

    add updated Merge-Sort

--- 01-Algo/16-Merge-Sort.js
+++ 01-Algo/16-Merge-Sort.js
@@ -1,18 +1,48 @@
 // https://hackernoon.com/programming-with-js-merge-sort-deb677b777c0
 
-let h1 = arr.splice(0, Math.floor(arr.length / 2))
-let h2 = arr
+// ---------------------------------------------------------
+//   This function concatenate the arrays by value order
+const joinarr = ( h1, h2 ) => {
+  let result = []
 
-console.log(h1);
-console.log(h2);
+  while ( h1.length || h2.length ) {
+    if(h1[0] < h2[0] || h2[0] === undefined && h1[0] !== undefined ){ result.push( h1.shift()) }
+    if(h1[0] > h2[0] || h1[0] === undefined && h2[0] !== undefined ){ result.push( h2.shift()) }
+  }
 
-h1 = myMerge(h1)
-h2 = myMerge(h2)
+  return result
+}
 
 
+// ---------------------------------------------------------
+//   This is the recursive function that splits the array until it has only single items arrays
+const myMerge = (arr) => {
+
+  if(arr.length === 1){ return arr }
+
+  let h1 = arr.splice(0, Math.floor(arr.length / 2))
+  let h2 = arr
+
+  return joinarr( myMerge(h1), myMerge(h2) )
+}
+
+
+
+// ---------------------------------------------------------
+//   Callgin the primary recursive functions
+const arr = [1, 9, 6, 5, 8, 3, 7, 0, 2, 4]
+console.log( myMerge(arr) )   // [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+
+// -------------------------------------------------------------------------------------
 
 
 // =============================================================================================
+// =============================================================================================
+// =============================================================================================
+
+
+
 // MERGE-SORT
 // If you have two sorted arrays, how you can conbinethem and the product be sorted.
 
--- 06-JSF/03-JS-Test/one.js
+++ 06-JSF/03-JS-Test/one.js
@@ -1,37 +1,39 @@
+// ---------------------------------------------------------
+//   This function concatenate the arrays by value order
+const joinarr = ( h1, h2 ) => {
+  let result = []
+
+  while ( h1.length || h2.length ) {
+    if(h1[0] < h2[0] || h2[0] === undefined && h1[0] !== undefined ){ result.push( h1.shift()) }
+    if(h1[0] > h2[0] || h1[0] === undefined && h2[0] !== undefined ){ result.push( h2.shift()) }
+  }
+
+  return result
+}
 
 
-// const joinarr = ( h1, h2 ) => {
-//   let result = []
-//
-//   while ( h1.length || h2.length ) {
-//     if(h1[0] < h2[0] || h2[0] === undefined && h1[0] !== undefined ){ result.push( h1.shift()) }
-//     if(h1[0] > h2[0] || h1[0] === undefined && h2[0] !== undefined ){ result.push( h2.shift()) }
-//   }
-//
-//   return result
-// }
-//
-//
-// // ---------------------------------------------------------
-//
-// const myMerge = (arr) => {
-//
-//   if(arr.length === 1){ return arr }
-//
-//   let h1 = arr.splice(0, Math.floor(arr.length / 2))
-//   let h2 = arr
-//
-//   return joinarr( myMerge(h1), myMerge(h2) )
-// }
-//
-//
-//
-// // ---------------------------------------------------------
-//
-// const arr = [1, 9, 6, 5, 8, 3, 7, 0, 2, 4]
-// console.log( myMerge(arr) )   // [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+// ---------------------------------------------------------
+//   This is the recursive function that splits the array until it has only single items arrays
+const myMerge = (arr) => {
+
+  if(arr.length === 1){ return arr }
+
+  let h1 = arr.splice(0, Math.floor(arr.length / 2))
+  let h2 = arr
+
+  return joinarr( myMerge(h1), myMerge(h2) )
+}
+
+
+
+// ---------------------------------------------------------
+//   Callgin the primary recursive functions
+const arr = [1, 9, 6, 5, 8, 3, 7, 0, 2, 4]
+console.log( myMerge(arr) )   // [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
 
 // -------------------------------------------------------------------------------------
+
 // let arr1 = []
 // let arr2 = [4]
 //
@@ -44,10 +46,3 @@
 // if(arr1.length){ console.log("No Vacio"); } True
 // if(arr2.length){ console.log("arr2 empty"); } False
 // -------------------------------------------------------------------------------------
-
-const myRec = (str, num) => {
-  if( num >= str.length ){ return num }
-  return myRec(str, num + 1)
-}
-
-console.log("Chars in string: ", myRec("1234567", 0) )
