90933b13a32623dd9f154f156e46c1b99eb6102a
Edxael <zerphyr.r@gmail.com>
2018-02-04 13:33:53 -0700

    regex negation

--- 03-Regex/01-basic-examples.js
+++ 03-Regex/01-basic-examples.js
@@ -3,16 +3,22 @@
 // Online:  https://regex101.com/
 
 =============================================================
-specific characters, case sensitive
-search for only
-[aAbB]
-abcdeABCDaEoiuuyBsdrs
+-specific characters, case sensitive
+[aAbB]  <--- search for only the characters inside.
+
 
 
 =============================================================
-Ranges
+-Ranges
 [a-zA-Z]  <--- all leters no matter the case.
 [a-zA-Z0-9]  <--- all leters no matter the case & all the numbers
+
+
+
+=============================================================
+-Negation:
+[^0-9] <--- Every thing that is not a number.
+
 
 
 =============================================================
