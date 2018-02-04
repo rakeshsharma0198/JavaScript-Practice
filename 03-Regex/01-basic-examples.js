d503967767807c0c2a4b18a8275d96453ea825f3
Edxael <zerphyr.r@gmail.com>
2018-02-04 13:28:48 -0700

    regex numeric case

--- 03-Regex/01-basic-examples.js
+++ 03-Regex/01-basic-examples.js
@@ -12,6 +12,7 @@ abcdeABCDaEoiuuyBsdrs
 =============================================================
 Ranges
 [a-zA-Z]  <--- all leters no matter the case.
+[a-zA-Z0-9]  <--- all leters no matter the case & all the numbers
 
 
 =============================================================
