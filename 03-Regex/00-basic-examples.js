4675e9abaaca3a6c6831e093a9e5a544308057ce
Edxael <zerphyr.r@gmail.com>
2018-02-04 14:13:17 -0700

    add regex metacharacters

--- 03-Regex/00-basic-examples.js
+++ 03-Regex/00-basic-examples.js
@@ -11,7 +11,7 @@
 =============================================================
 -Ranges
 [a-zA-Z]  <--- all leters no matter the case.
-[a-zA-Z0-9]  <--- all leters no matter the case & all the numbers
+[a-zA-Z0-9_]  <--- all leters no matter the case & all the numbers
 
 
 
@@ -34,4 +34,36 @@
 
 =============================================================
 -Matching all characters from spce" " to tilda"~" (this is a selection for the ASKII)
-[\ -~] <--- 1st escape the space, the do the match. 
+[\ -~] <--- 1st escape the space, the do the match.
+
+
+
+=============================================================
+-Alterations
+cat | dog | tiger
+
+cat dog tiger lion
+
+
+
+=============================================================
+-Metacharacters (this list is not complete...)
+
+  " . " The periond means anithing, but if you really want to mathc for the period, then you need to escape it.
+ .   <--- Anything get match.
+ \.  <--- Only the periods get match
+[.]  <--- Only the periods get match
+
+
+ [a-zA-Z0-9_]  ===  \w   <--- all leters no matter the case & all the numbers
+[^a-zA-Z0-9_]  ===  \W   <--- everitthing exept { all leters no matter the case & all the numbers }
+
+ [0-9] ===  \d   <--- Select all the digits.
+[^0-9] ===  \D   <--- Match anyting but the digits.
+
+\t    <--- Match "TABS"
+\n    <--- Line breaks.
+\s    <--- Match all spaces
+`\ `  <--- Match all spaces, I wrap it in Backtis  so you can see the spcae but you not need it.
+\S    <--- Match anything that is not a "SPACE"
+\[\]  <--- Match all opeining and closing square braces.
