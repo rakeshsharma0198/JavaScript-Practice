564366c68d2a6a78ef8c7d19a966e99c231da32c
Edxael <zerphyr.r@gmail.com>
2018-02-04 13:25:24 -0700

    regex case

--- 03-Regex/01-basic-examples.js
+++ 03-Regex/01-basic-examples.js
@@ -1,2 +1,14 @@
 
 // Basic Examples of REGEX
+// Online:  https://regex101.com/
+
+=============================================================
+specific characters, case sensitive
+search for only
+[aAbB]
+abcdeABCDaEoiuuyBsdrs
+
+=============================================================
+
+
+=============================================================
