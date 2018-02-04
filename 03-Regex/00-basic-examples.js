24cd2e46be9e4f0c768fb082caa2a915283ec3d3
Edxael <zerphyr.r@gmail.com>
2018-02-04 13:47:02 -0700

    from space to tilda

--- 03-Regex/00-basic-examples.js
+++ 03-Regex/00-basic-examples.js
@@ -20,9 +20,18 @@
 [^0-9] <--- Every thing that is not a number.
 [^a-z] <--- Every thing that is not a lowercase letter.
 
+  Note: the carrot only have this negation meaning when is the first character inside the brackets. else: is a character to be match.
+
 
 
 =============================================================
 -Searching for a dash " - "
 [\-] <--- select only the dashes.
 [\-a-z]  <--- All dashes and All lowercase letters.
+[a-z-]   <--- All dashes and All lowercase letters.
+
+
+
+=============================================================
+-Matching all characters from spce" " to tilda"~" (this is a selection for the ASKII)
+[\ -~] <--- 1st escape the space, the do the match. 
