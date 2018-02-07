12ff1a92e4da32389fbea35aa9b1af37b38dc07d
Edmundo Rubio <zerphyr.r@gmail.com>
2018-02-06 18:10:10 -0700

    add notes

--- 06-JSF/04-Class/code.js
+++ 06-JSF/04-Class/code.js
@@ -1,1 +1,10 @@
-console.log("Technical");
+console.log("classl");
+
+var myArray = ["H", "e", "l", "l", "0"];
+console.log(myArray);
+
+var arr2 = myArray.splice(0, 3)
+
+console.log("");
+console.log(myArray);
+console.log(arr2);
