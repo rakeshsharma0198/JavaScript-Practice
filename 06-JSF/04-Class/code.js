60a4818061a61c64e398a97029dc78aa6ba6b2b5
Edxael <zerphyr.r@gmail.com>
2018-02-09 14:41:42 -0700

    add tutorial point

--- 06-JSF/04-Class/code.js
+++ 06-JSF/04-Class/code.js
@@ -1,5 +1,5 @@
 console.log("Docmumentation...");
 // Class:
 // 01- Review Coding Enviroment
-// 02- Review Arrays.
+// 02- Review Arrays.   TutorialsPoint:  https://www.tutorialspoint.com/javascript/javascript_arrays_object.htm
 // 03- Functions.
