00e7f4dc2287d390bcf6dda8a63e1a1747c28286
Edxael <zerphyr.r@gmail.com>
2018-02-09 14:43:48 -0700

    add notes on coding enviroment

--- 06-JSF/04-Class/code.js
+++ 06-JSF/04-Class/code.js
@@ -1,5 +1,5 @@
 console.log("Docmumentation...");
 // Class:
-// 01- Review Coding Enviroment
+// 01- Review Coding Enviroment:  Atom Text Editor, Chrome Browser (Console.) 
 // 02- Review Arrays.   TutorialsPoint:  https://www.tutorialspoint.com/javascript/javascript_arrays_object.htm
 // 03- Functions.
