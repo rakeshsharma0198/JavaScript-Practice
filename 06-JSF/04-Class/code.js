43828cba68920c895dd9b5c8bd34e00849ba8a70
Edxael <zerphyr.r@gmail.com>
2018-02-09 20:30:14 -0700

    update html file

--- 06-JSF/04-Class/code.js
+++ 06-JSF/04-Class/code.js
@@ -1,13 +1,7 @@
 console.log("Docmumentation...");
 
 
-// 00- Waiting for late people. --- 7 minutes.
-// 01- explanation of Internet & web-pages ( Internet, Hosting, HTML, CSS, JavaScript ) --- 15 minutes.
-// 02- Web-Development Enviroment ( Atom, Browser, Console ) --- 15 minutes.
-// 03- Practice enviroment exersice ( create a project: H1, p, background color, center, console.log("text.") ) --- 15 minutes.
-// 04- Break ( bathrom etc. ) 7 minutes.
-// 05- Web-Page Add 2 numbers ( Code Along. ) --- 90 minutes
-// 06- Break ( bathrom etc. ) 7 minutes.
-// 07- JavaScript: Functions, Arrays, For Loop. --- 35 minutes.
-//
-// Homework: watch the add two numbers and doit again.
+
+
+
+console.log("Hello from here");
