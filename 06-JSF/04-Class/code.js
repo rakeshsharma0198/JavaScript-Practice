6fc98884ea648d56e11b83cc6b86ac953509a5b3
Edxael <zerphyr.r@gmail.com>
2018-02-09 22:30:31 -0700

    add css and js files

--- 06-JSF/04-Class/code.js
+++ 06-JSF/04-Class/code.js
@@ -1,7 +1,16 @@
 console.log("Docmumentation...");
 
 
+function addNumbers (){
+  console.log("Hello from inside the function.....");
+
+  var data1 = parseInt( document.getElementById("in1").value )
+  var data2 = parseInt( document.getElementById("in2").value )
+
+  var total = data1 + data2
+
+  document.getElementById("out").value = total
 
 
-
-console.log("Hello from here");
+  console.log("The sum of the numbers is: ", total);
+}
