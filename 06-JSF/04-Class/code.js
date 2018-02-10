ad4ce5428f622e13e310801ca9da9d20dcf6e384
Edmundo Rubio <zerphyr.r@gmail.com>
2018-02-10 09:53:47 -0700

    add notes

--- 06-JSF/04-Class/code.js
+++ 06-JSF/04-Class/code.js
@@ -1,29 +1,17 @@
-<<<<<<< HEAD
 console.log("classl");
-
-var myArray = ["H", "e", "l", "l", "0"];
-console.log(myArray);
-
-var arr2 = myArray.splice(0, 3)
-
-console.log("");
-console.log(myArray);
-console.log(arr2);
-=======
-console.log("Docmumentation...");
 
 
 function addNumbers (){
   console.log("Hello from inside the function.....");
 
-  var data1 = parseInt( document.getElementById("in1").value )
-  var data2 = parseInt( document.getElementById("in2").value )
+  var data1 = document.getElementById("in1").value
+  var data2 = document.getElementById("in2").value
+
 
   var total = data1 + data2
 
   document.getElementById("out").value = total
 
 
-  console.log("The sum of the numbers is: ", total);
+  console.log("The sum of the numbers is: ", total, " and is type is: ", typeof total);
 }
->>>>>>> 6fc98884ea648d56e11b83cc6b86ac953509a5b3
