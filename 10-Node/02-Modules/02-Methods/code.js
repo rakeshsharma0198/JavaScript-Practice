9af526259b09e75db01aee72a4fda6c4595bec22
Edxael <zerphyr.r@gmail.com>
2018-02-11 15:35:04 -0700

    create magic library

---
+++ 10-Node/02-Modules/02-Methods/code.js
@@ -0,0 +1,8 @@
+const magic = require('./magic')
+
+let data = 48
+
+console.log( magic.doubleIt(data) );
+console.log( magic.tripeIt(data) );
+console.log( magic.halfIt(data) );
+console.log( magic.zeroIt(data) );
---
+++ 10-Node/02-Modules/02-Methods/magic/index.js
@@ -0,0 +1,7 @@
+
+module.exports = {
+  doubleIt: (num) => { return num * 2 },
+  halfIt: (num) => { return num / 2 },
+  tripeIt: (num) => { return num *3 },
+  zeroIt: (num) => { return num * 0 }
+}
