2c085049f057dbb7403a1f4ba621d3a184224ada
Edxael <zerphyr.r@gmail.com>
2018-02-11 20:19:00 -0700

    add app

--- 10-Node/04-Express/02-ejs/server.js
+++ 10-Node/04-Express/02-ejs/server.js
@@ -5,24 +5,27 @@ const morgan = require('morgan')
 
 
 // ------------------------------------------------------
-app.use('view engine', 'ejs')
-
-app.use(express.static(__dirname + '/node_modules/bootstrap/dist'))
-app.use(express.static(__dirname + '/views'))
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/node_modules/bootstrap/dist'));
+app.use(express.static(__dirname + '/views'));
 
 
 // ------------------------------------------------------
 app.get('/', (req, res) => {
-  res.render('pages/00-home.ejs')
-})
+    res.render('pages/00-home.ejs');
+});
 
 app.get('/1', (req, res) => {
   res.render('pages/01-aboutUs.ejs')
+})
+
+app.get('/2', (req, res) => {
+  res.render('pages/02-contacttUs.ejs')
 })
 
 
 // ------------------------------------------------------
 app.listen((process.env.PORT || 3000), (err) => {
   if(err){ throw err }
-  console.log("\n Server Running.........................")
+  console.log("\n\n=========================\nServer Running....\n");
 })
