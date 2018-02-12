135b386913f3c7bf5ace0915472e75f8a219fbef
Edxael <zerphyr.r@gmail.com>
2018-02-12 10:30:31 -0700

    add fizz practice

--- 01-JS-Algo/03-fizz-buzz.js
+++ 01-JS-Algo/03-fizz-buzz.js
@@ -14,3 +14,20 @@ const fizz = (num) => {
 }
 
 console.log( fizz(20) );
+
+
+// --------------------------------------------------------------------------
+// Practice Code
+
+const fizz = (num) => {
+  let data = new Array(num)
+                .fill('a')
+                .map((c, i) => { return i +1 })
+                .map((x) => { return (x % 3 === 0 && x % 5 === 0) ? `The number: ${x} is Fizz-Buzz` :
+                  (x % 3 === 0) ? `The number: ${x} is Fizz` :
+                  (x % 5 === 0) ? `The number: ${x} is Buzz` : `The number: ${x} N/A`
+                 })
+  return data
+}
+
+console.log( fizz(15) )
--- 01-JS-Algo/one.js
+++ 01-JS-Algo/one.js
@@ -1,68 +1,15 @@
 
 console.log("GT 24")
 
-// ============================================================================================
-//                                             CODE
-// ============================================================================================
-// Giving an input of 325489 the nesesary steps are: 552
-
-const grid = (num) => {
-
-  // Create a variables
-  let steps = 0
-  let counter = 0
-  let middleValue = 0
-  let breakPoints = []
-  let cornerValues = []
-  // -----------------------------------------------------------------------------------------
-
-  // Load array "BreackPoints" with values, example: [1, 9, 25, 49, ...]
-  do {
-      breakPoints.push( (2 * counter +1)**2 )
-      counter += 1
-  }while (breakPoints[breakPoints.length -1] <= num)
-  if(breakPoints[breakPoints.length -2] === num){ breakPoints.pop() }
-  // -----------------------------------------------------------------------------------------
-
-
-  // Load array "Corner-Vales" with values from last "BreackPoint" example: 49 = [49, 43, 37, 31]
-  counter = breakPoints[breakPoints.length -1]
-  for(let x = 0; x < 4; x++){
-    cornerValues.push(counter)
-    counter = counter - (Math.sqrt( breakPoints[breakPoints.length -1] ) - 1)
-  }
-  // -----------------------------------------------------------------------------------------
-
-
-  // Identify "Corner-Value" that is greather or equal to "Given-Number".
-  cornerValues.forEach((x) => {
-    if(num <= x){ counter = x }
-  })
-  // -----------------------------------------------------------------------------------------
-
-
-  // Calculate "Midle-Value" of chosen "Corner-Value" range.
-  middleValue = counter - Math.floor( Math.sqrt( breakPoints[breakPoints.length -1] ) / 2 )
-  // -----------------------------------------------------------------------------------------
-
-
-  // Calculate the proximyty "Given-Number" to "Midle-Value" of its "Corner-Value", and save it on "steps"
-  if(num < middleValue){ steps = middleValue - num }
-  if(num > middleValue){ steps = num - middleValue }
-  // -----------------------------------------------------------------------------------------
-
-
-  // Add to steps 1 for each remaining "BreackPoints".
-  steps += ( breakPoints.length -1 )
-  // -----------------------------------------------------------------------------------------
-
-
-  // Return steps
-  return steps
+const fizz = (num) => {
+  let data = new Array(num)
+                .fill('a')
+                .map((c, i) => { return i +1 })
+                .map((x) => { return (x % 3 === 0 && x % 5 === 0) ? `The number: ${x} is Fizz-Buzz` :
+                  (x % 3 === 0) ? `The number: ${x} is Fizz` :
+                  (x % 5 === 0) ? `The number: ${x} is Buzz` : `The number: ${x} N/A`
+                 })
+  return data
 }
 
-
-// *************** Input & Calling the Function ***************
-// console.log( grid(325489) )
-console.log( grid(325489) )
-// **********************************************************************************************
+console.log( fizz(15) )
