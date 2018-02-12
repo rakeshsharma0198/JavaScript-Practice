d29f10734cd4310e387f6c7c7d2f1ed9cbbdffbb
Edxael <zerphyr.r@gmail.com>
2018-02-12 12:57:03 -0700

    new fibonacy recursive logic

--- 01-JS-Algo/13-fib-rec.js
+++ 01-JS-Algo/13-fib-rec.js
@@ -1,6 +1,26 @@
 
 // Exponential run Time
 
+function fib(n) {
+ count = 0
+ let arr = [0, 1]
+
+ let myFib = (count) => {
+   if(count === n - 1){ return }
+   arr.push(arr[count] + arr[count + 1])
+   myFib(count + 1)
+ }
+
+ myFib(count)
+ return arr
+}
+
+
+console.log(fib(10));
+
+
+// --------------------------------------------------
+
 const fibRec2 = (position) => {
   if(position < 3) { return 1}
   else{ return fibRec2(position - 1) + fibRec2(position - 2) }
--- 01-JS-Algo/one.js
+++ 01-JS-Algo/one.js
@@ -1,12 +1,20 @@
 
 console.log("GT 25")
 
-// split into an array of words
-// eachword to array and reverse and to string
-// Join wit a space(" ")
 
-const revWords = (str) => {
+function fib(n) {
+ count = 0
+ let arr = [0, 1]
 
+ let myFib = (count) => {
+   if(count === n - 1){ return }
+   arr.push(arr[count] + arr[count + 1])
+   myFib(count + 1)
+ }
+
+ myFib(count)
+ return arr
 }
 
-console.log( revWords("Coding Javascript") )
+
+console.log(fib(10));
