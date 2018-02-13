48f11532c5ea0112169b35b98d5d45a3dbb5c384
Edxael <zerphyr.r@gmail.com>
2018-02-13 15:30:17 -0700

    React add 2 Nums

---
+++ 04-React-Apps/01-add2nums/src/01-Add.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default class AddNums extends React.Component {
+  render() {
+    return(
+      <div>Hello from Add.jsx</div>
+    )
+  }
+}
---
+++ 04-React-Apps/01-add2nums/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import AddComponent from './01-Add.jsx'
+
+class IndexComp extends React.Component  {
+  render() {
+    return(
+      <div>
+        <div>Hello from index</div>
+        <AddComponent />
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<IndexComp />, document.getElementById("root"))
