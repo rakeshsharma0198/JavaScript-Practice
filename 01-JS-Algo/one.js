44a32a5d623242480d5ed96b3bece36c03876405
Edxael <zerphyr.r@gmail.com>
2018-02-13 20:33:37 -0700

    update files

--- 01-JS-Algo/one.js
+++ 01-JS-Algo/one.js
@@ -17,3 +17,20 @@ const dup = (arr) => {
     console.log(myArr);
     
     console.log(dup(myArr))
+
+
+
+    function isPalindrome(string) {
+        string = string.toLowerCase();
+        var charactersArr = string.split('');
+        var validCharacters = 'abcdefghijklmnopqrstuvwxyz'.split('');
+      
+        var lettersArr = [];
+        charactersArr.forEach(char => {
+          if (validCharacters.indexOf(char) > -1) lettersArr.push(char);
+        });
+      
+        return lettersArr.join('') === lettersArr.reverse().join('');
+      }
+      
+      isPalindrome("Madam, I'm Adam");
