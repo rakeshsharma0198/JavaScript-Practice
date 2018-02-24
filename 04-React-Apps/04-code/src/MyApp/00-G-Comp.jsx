93b78f08086258ae33b12d145716743db448eaa4
Edxael <zerphyr.r@gmail.com>
2018-02-23 22:47:12 -0700

    update component

--- 04-React-Apps/04-code/src/MyApp/00-G-Comp.jsx
+++ 04-React-Apps/04-code/src/MyApp/00-G-Comp.jsx
@@ -4,10 +4,13 @@ export default class extends React.Component{
     render(){
         return(
             <div>
+
                <h1>React.js Practice</h1>
 
                <input type="text"/>
                <br/><br/>
+
+                <input type="text"/>
 
             </div>
         )
