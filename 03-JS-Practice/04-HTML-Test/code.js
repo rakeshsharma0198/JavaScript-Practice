e566a4d9355fe3abbb96dab5b32c2abb800dc4c9
Edxael <zerphyr.r@gmail.com>
2018-02-24 20:54:59 -0700

    add js file

--- 03-JS-Practice/04-HTML-Test/code.js
+++ 03-JS-Practice/04-HTML-Test/code.js
@@ -9,12 +9,16 @@ var list = []
 
 
 console.log(list);
+
 function  Alphabetize(){
   list[0] = document.getElementById('1').value
   list[1] = document.getElementById('2').value
   list[2] = document.getElementById('3').value
+
   console.log(list)
+
   var sorted = list.sort()
+  
   document.getElementById('1').value = list[0]
   document.getElementById('2').value = list[1]
   document.getElementById('3').value = list[2]
